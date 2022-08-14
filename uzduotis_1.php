ec49d282aec1c005d980b903eaf792a0227841f4
Herasil <78405491+Herasil@users.noreply.github.com>
2022-08-15 01:04:50 +0300

    Get array header method

--- uzduotis_1.php
+++ uzduotis_1.php
@@ -9,7 +9,16 @@ $searchForSymbols = array("array", "(", ")", "'","\r\n",";"," ");
 $rawParsedDataFromFile = explode(',',str_replace($searchForSymbols, '', $file));
 print_r($rawParsedDataFromFile);
 
-
+function getHeader(): array {
+    $arrHeader = [];
+    foreach($rawParsedDataFromFile as $item){
+        $singleParsedDataLine = explode('=>',$item);
+        array_push($arrHeader,$singleParsedDataLine[0]);
+    }
+    unset($arrHeader[sizeof($arrHeader)-1]);
+    $headerFull = array_unique($arrHeader);
+    return $headerFull;
+}
 
 
 
